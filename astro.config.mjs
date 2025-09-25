a04a1c625396d70f11afcc0385f746afa8d810d9
Niek Kemp <niekkemp2002@gmail.com>
2025-09-25 16:09:14 +0200

    COmmit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-// 
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://niekkemp.github.io/aion-web',
+  site: 'https://niekkemp.github.io',
   base: '/aion-web',
   vite: {
     plugins: [tailwindcss()]
